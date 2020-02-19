afe45ca6b86885ed0d3d957e87c48fcbd8eab3b0
gouveiarocha <gouveia.rocha@hotmail.com>
2020-02-19 15:02:16 -0300

    add config main interface

--- app/src/main/java/com/example/organizzeclone/Activitys/PrincipalActivity.java
+++ app/src/main/java/com/example/organizzeclone/Activitys/PrincipalActivity.java
@@ -9,18 +9,30 @@ import com.example.organizzeclone.Modelo.Usuario;
 import com.github.clans.fab.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.organizzeclone.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
+
+    private TextView txtSaudacao, txtSaldo;
+    private MaterialCalendarView materialCalendarView;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +40,31 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Visão Geral");
         setSupportActionBar(toolbar);
 
+        //REF. Componentes
+        txtSaudacao = findViewById(R.id.txt_saudacao);
+        txtSaldo = findViewById(R.id.txt_saldo);
+        materialCalendarView = findViewById(R.id.calendar_view);
+        recyclerView = findViewById(R.id.recycler_view_principal);
+
+        iniciarCalendario();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_sair){
+            deslogar();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void adicionarReceita(View view){
@@ -39,6 +74,33 @@ public class PrincipalActivity extends AppCompatActivity {
     public void adicionarDespesa(View view){
         startActivity(new Intent(this,DespesasActivity.class));
     }
+
+    public void iniciarCalendario(){
+
+        //Configurações Calendário...
+        CharSequence meses[] = {"JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ"};
+        materialCalendarView.setTitleMonths(meses);
+
+        //Recuperar Mes/Ano ao trocar o Mês...
+        materialCalendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                Toast.makeText(PrincipalActivity.this, date.getMonth() + "/" + date.getYear(), Toast.LENGTH_SHORT).show(); //mes+ano
+            }
+        });
+
+    }
+
+    //Método para Deslogar Usuario.
+    public void deslogar(){
+        auth = ConfigFirebase.getAuth();
+        auth.signOut();
+        finish();
+    }
+
+    /**
+     * Não utilizados...
+     */
 
     //Exemplo de ação dos menus usando o View.OnclickListener. Mas para um código mais limpo, usaremos o onClick para as ações dos botões.
     public void acaoMenus() {
@@ -61,11 +123,4 @@ public class PrincipalActivity extends AppCompatActivity {
 
     }
 
-    public void deslogar(View v){
-
-        auth = ConfigFirebase.getAuth();
-        auth.signOut();
-        finish();
-
-    }
 }
